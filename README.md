319ae3ea258406f137c270cee41a91459060d710
joao-dias-25 <joaopbdias@gmail.com>
2020-11-23 15:40:11 +0100

    update

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# Stats-ML
-App
+# Sentiment analysis 
+
+### tasks:
+- create an option to select language model
+- tokenizing the text with spacy
+- understanding the Name Entities Recognition
+- train model with Keras
+- deploy in heruko app 
--- stats_ml.py
+++ stats_ml.py
@@ -9,7 +9,11 @@ import pandas as pd
 
 
 SPACY_MODEL_NAMES = ["en_core_web_sm", "en_core_web_md", "de_core_news_sm", "pt_core_news_sm"]
-DEFAULT_TEXT = "Some random text that I wrote."
+DEFAULT_TEXT = """New York City on Tuesday declared a public health emergency and ordered mandatory measles vaccinations amid an outbreak, becoming the latest national flash point over refusals to inoculate against dangerous diseases.
+
+At least 285 people have contracted measles in the city since September, mostly in Brooklyn’s Williamsburg neighborhood. The order covers four Zip codes there, Mayor Bill de Blasio (D) said Tuesday.
+
+The mandate orders all unvaccinated people in the area, including a concentration of Orthodox Jews, to receive inoculations, including for children as young as 6 months old. Anyone who resists could be fined up to $1,000."""
 HTML_WRAPPER = """<div style="overflow-x: auto; border: 1px solid #e6e9ef; border-radius: 0.25rem; padding: 1rem; margin-bottom: 2.5rem">{}</div>"""
 
 @st.cache(allow_output_mutation=True)
@@ -37,6 +41,29 @@ with st.echo():
     # tokenizing words
     doc = process_text(spacy_model, text)
 
+if "parser" in nlp.pipe_names:
+    st.header("Dependency Parse & Part-of-speech tags")
+    st.sidebar.header("Dependency Parse")
+    split_sents = st.sidebar.checkbox("Split sentences", value=True)
+    collapse_punct = st.sidebar.checkbox("Collapse punctuation", value=True)
+    collapse_phrases = st.sidebar.checkbox("Collapse phrases")
+    compact = st.sidebar.checkbox("Compact mode")
+    options = {
+        "collapse_punct": collapse_punct,
+        "collapse_phrases": collapse_phrases,
+        "compact": compact,
+    }
+    #displacy.render(doc, style = "dep",jupyter = True)
+
+if "ner" in nlp.pipe_names:
+    st.header("Text with Named Entities")
+    st.sidebar.header("Named Entities")
+    label_set = nlp.get_pipe("ner").labels
+    labels = st.sidebar.multiselect(
+        "Entity labels", options=label_set, default=list(label_set)
+    )
+    html = displacy.render(doc, style="ent", options={"ents": labels})
+    st.write(HTML_WRAPPER.format(html), unsafe_allow_html=True)
 
 
 
